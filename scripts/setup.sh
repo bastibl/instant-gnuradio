a476fd2256f6692429706a3f5175fbc26b668365
Bastian Bloessl <mail@bastibl.net>
2018-01-15 09:14:24 +0100

    go on

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -15,31 +15,30 @@ sudo pip install --upgrade pip
 
 pip install --user PyBOMBS
 
-# pybombs recipes add gr-recipes git+https://github.com/gnuradio/gr-recipes.git
-# pybombs recipes add gr-etcetera git+https://github.com/gnuradio/gr-etcetera.git
-# pybombs prefix init /home/gnuradio/pybombs -a myprefix
-# pybombs config default_prefix myprefix
-# pybombs config satisfy_order src,native
-# pybombs install gnuradio
+pybombs recipes add gr-recipes git+https://github.com/gnuradio/gr-recipes.git
+pybombs recipes add gr-etcetera git+https://github.com/gnuradio/gr-etcetera.git
+pybombs prefix init /home/gnuradio/pybombs/master -a master
+pybombs prefix init /home/gnuradio/pybombs/next -a next
+pybombs config default_prefix master
+pybombs config satisfy_order src,native
 
-# pybombs install limesuite
-# pybombs install gqrx
-# cd ~/pybombs/src/gr-osmosdr/build/
-# git remote add argilo https://github.com/argilo/gr-osmosdr.git
-# git fetch argilo
-# git checkout allow-tr-switching
-# . ~/pybombs/setup_env.sh
-# make && make install
-# ~/pybombs/lib/uhd/utils/uhd_images_downloader.p
+pyboms install rtl-sdr
+sudo cp pybombs/master/src/rtl-sdr/rtl-sdr.rules /etc/udev/rules.d/
 
-# sudo cp pybombs/src/osmo-sdr/software/libosmosdr/osmosdr.rules /etc/udev/rules.d/
-# sudo cp pybombs/src/airspy/airspy-tools/52-airspy.rules /etc/udev/rules.d/
-# sudo cp pybombs/src/rtl-sdr/rtl-sdr.rules /etc/udev/rules.d/
-# sudo cp pybombs/src/uhd/host/utils/uhd-usrp.rules /etc/udev/rules.d/
-# sudo cp pybombs/src/hackrf/host/libhackrf/53-hackrf.rules /etc/udev/rules.d/
-# sudo cp pybombs/src/limesuite/udev-rules/64-limesuite.rules /etc/udev/rules.d/
-# sudo sed s/@BLADERF_GROUP@/plugdev/ pybombs/src/bladeRF/host/misc/udev/88-nuand.rules.in > /etc/udev/rules.d/88-nuand.rules
+pyboms install hackrf
+sudo cp pybombs/master/src/hackrf/host/libhackrf/53-hackrf.rules /etc/udev/rules.d/
 
+pybombs install uhd
+sudo cp pybombs/master/src/uhd/host/utils/uhd-usrp.rules /etc/udev/rules.d/
+pybombs/master/lib/uhd/utils/uhd_images_downloader.py
+
+pybombs install gnuradio
+pybombs install gqrx
+pybombs install gr-osmosdr
+pybombs install gr-fosphor
+pybombs install gr-foo
+pybombs install gr-ieee-80211
+pybombs install gr-ieee-802154
 
 ### WIRESHARK
 echo "wireshark-common wireshark-common/install-setuid boolean true" | sudo debconf-set-selections
