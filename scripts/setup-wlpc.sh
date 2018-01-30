14a0d7ccccf3262bfed0821743e038d1fa68fe89
Bastian Bloessl <mail@bastibl.net>
2018-01-30 14:52:37 +0000

    wlpc cleanup

--- scripts/setup-wlpc.sh
+++ scripts/setup-wlpc.sh
@@ -4,8 +4,5 @@ set -exu
 
 git clone https://github.com/adriangranados/dfs-pulse-tester.git src/dfs-pulse-tester
 
-find ./pybombs -type d -name '.git' | xargs rm -rf
-find ./pybombs -type d -name 'build' | xargs rm -rf
-
 find ./src -type d -name '.git' | xargs rm -rf
 find ./src -type d -name 'build' | xargs rm -rf
