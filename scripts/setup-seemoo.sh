112be3b2a2e01273f09f020436af41429c96fcaa
Bastian Bloessl <mail@bastibl.net>
2020-11-07 15:07:41 +0100

    seemoo cricom vm

---
+++ scripts/setup-seemoo.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -eux
+
+export LANG=en_US.UTF-8
+export PATH="$HOME/.local/bin:${PATH}"
+
+
+cd ~/cricom/gr-powerswitch
+mkdir build
+cd build
+cmake ..
+make
+sudo make install
+sudo ldconfig
+
