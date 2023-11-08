f431817323379f05eae8749839fdf6a8a2bf3261
Bastian Bloessl <mail@bastibl.net>
2023-11-08 09:05:58 +0100

    asdf
    # Conflicts:
    #	scripts/setup-gnuradio.sh

--- scripts/setup-sdr.sh
+++ scripts/setup-sdr.sh
@@ -35,7 +35,7 @@ echo "sdr - rtprio 99" | sudo tee -a /etc/security/limits.conf
 sudo apt-get update
 sudo apt-get -y upgrade
 
-sudo apt-get -y install jupyter jupyter-qtconsole jupyter-notebook python3-matplotlib python3-ipython python3-scipy python3-numpy python3-pip python3-requests multimon-ng sox liborc-dev gr-fosphor gr-osmosdr gqrx-sdr inspectrum hackrf soapysdr-tools libsoapysdr-dev soapysdr0.8-module-{bladerf,hackrf,osmosdr,rtlsdr,uhd} gnuradio gr-rds gr-satellites rtl-sdr bladerf jq
+sudo apt-get -y install jupyter jupyter-qtconsole jupyter-notebook python3-matplotlib python3-ipython python3-scipy python3-numpy python3-pip python3-requests multimon-ng sox liborc-dev gr-fosphor gr-osmosdr gqrx-sdr inspectrum hackrf soapysdr-tools libsoapysdr-dev soapysdr0.8-module-{bladerf,hackrf,osmosdr,rtlsdr,uhd} gnuradio gnuradio-dev gr-rds gr-satellites rtl-sdr bladerf jq
 
 sudo snap install urh
 
